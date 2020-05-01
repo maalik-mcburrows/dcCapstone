b2753e50300d822415fde2fe0d353718d11cca4b
Maalik McBurrows <mmcburrows1@gmail.com>
2020-05-01 03:03:21 -0400

    about to create new branch to fix bug. Testing sites returning array instead of string of individual site

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -24,69 +24,83 @@ class Case extends Component {
         this.getTestingSites(event.target.value);
     }
 
-    async componentDidMount() {
+    componentDidMount = async () => {
         this.getTestingSites(this.state.state);
+    }
+
+    onSiteChange = (event) => {
+        this.setState({
+            testing_site : [event.target.value]
+        })
+        console.log(this.state.testing_site)
     }
 
     getTestingSites = async(stateName) => {
         const testSiteUrl = await fetch(`https://covid-19-testing.github.io/locations/${stateName.toLowerCase()}/complete.json`);
         const response = await testSiteUrl.json();
         const siteNames = [];
-        const sites = response.map(site => {
+        response.map(site => {
             return siteNames.push(site.name);
         });
         console.log(siteNames);
         this.setState({
             testing_site : siteNames
-        })
+        });
         // console.log('test site =>', sites);
     }
 
     handleSubmit = (event) => {
         event.preventDefault()
         const data = this.state
-        const url = 'http://localhost:9000/case';
-        axios.post(url,data)
-        .then(response=>console.log(response))
-        .catch(e=>console.log(e))
+        console.log(data);
+        // const url = 'http://localhost:9000/case';
+        // axios.post(url,data)
+        // .then(response=>console.log(response))
+        // .catch(e=>console.log(e))
     }
-
-    
 
     render() {
 
         const stateOptions = ['Alabama' , 'Alaska' , 'Arizona' , 'Arkansas' , 'California' , 'Colorado' , 'Connecticut' , 'Delaware' , 'Florida' , 'Georgia' , 
         'Hawaii' , 'Idaho', 'Illinois', 'Indiana', 'Iowa' , 'Kansas' , 'Kentucky' , 'Louisiana' , 'Maine' , 'Maryland', 'Massachusetts' , 'Michigan' , 'Minnesota' , 'Mississippi', 'Missouri',
-        'Montana' , 'Nebraska' , 'Nevada' , 'New Hampshire' , 'New Jersey' , 'New Mexico' , 'New York' , 'North Carolina' , 'North Dakota' , 'Ohio', 'Oklahoma' , 'Oregon' , 'Pennsylvania' , 'Rhode Island' , 'South Carolina', 
-        'South Dakota' , 'Tennessee' , 'Texas' , 'Utah' , 'Vermont' , 'Virginia' , 'Washington' , 'West Virginia' , 'Wisconsin' , 'Wyoming' ]
+        'Montana' , 'Nebraska' , 'Nevada' , 'New-Hampshire' , 'New-Jersey' , 'New-Mexico' , 'New-York' , 'North-Carolina' , 'North-Dakota' , 'Ohio', 'Oklahoma' , 'Oregon' , 'Pennsylvania', 'South-Carolina', 
+        'Tennessee' , 'Texas' , 'Utah' , 'Vermont' , 'Virginia' , 'Washington' , 'West-Virginia' , 'Wisconsin' , 'Wyoming' ]
 
-        const { test_date, testing_site } = this.state
+        let { test_date, testing_site } = this.state
+        console.log('this the sites', testing_site);
         return (
             <div className="case">
                 <div className="caseTitle">
                     <h1>Have you tested positive for <span style={{color: "red"}}>COVID-19</span>? Please file an <span style={{color: "red"}}>anonymous</span> case report below.</h1>
                 </div>
                 <form className="caseForm" onSubmit={this.handleSubmit}>
-                    <div style={{paddingRight: "10px"}}>
-                        <input placeholder="Test Date" style={{borderRadius: "10px", textAlign: "right"}} type="date" name="test_date" value={test_date} onChange={this.handleChange} />
+                    <div className="caseInputLabel">TEST DATE : 
+                        <div className="caseInputDiv">
+                            <input className="inputElement"  style={{ borderRadius: "15px", textAlign: "right", fontStyle: "normal", fontFamily: "sans-serif", color: "black" }} type="date" name="test_date" value={test_date} onChange={this.handleChange}/>
+                        </div>
                     </div>
-                    <div style={{paddingRight: "10px"}}>
-                        {/* <input placeholder="State" style={{borderRadius: "10px", textAlign: "center"}} type="text" name="state" value={state} onChange={this.handleChange} /> */}
-                        <select onChange={this.stateHandleChange}>
-                            {stateOptions.map((stateOption, index) => (
-                                <option placeholder="State" style={{borderRadius: "10px", textAlign: "center"}} key={index} value={stateOption} name="state" >{stateOption}</option>
-                            ))}
-                        </select>
+                    <div className="caseInputLabel">STATE :
+                        <div className="caseInputDiv">
+                            {/* <input placeholder="State" style={{borderRadius: "10px", textAlign: "center"}} type="text" name="state" value={state} onChange={this.handleChange} /> */}
+                            <select className="inputElement"  style={{borderRadius: "10px", textAlign: "center", padding: "2.5px"}} onChange={this.stateHandleChange}>
+                                {stateOptions.map((stateOption, index) => (
+                                    <option key={index} value={stateOption} name="state" >{stateOption}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-                    
-                    <div style={{paddingRight: "10px"}}>
-                        <select onChange={this.handleChange}>
-                            {testing_site.map(site => (
-                                <option placeholder="Testing Location" style={{borderRadius: "10px", textAlign: "center"}} name="testing_site" value={site} >{site}</option>
-                            ))}
-                        </select>
+                    <div className="caseInputLabel">TEST SITE :
+                        <div className="caseInputDiv">
+                            <select className="inputElement" style={{borderRadius: "10px", textAlign: "center", padding: "2.5px"}} onChange={this.onSiteChange}>
+                                {testing_site.map((site, index) => (
+                                    <option key={index} name="testing_site" value={site} >{site}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-                    <button style={{borderRadius: "10px"}} type="submit"> SUBMIT </button>
+                    <div id="caseButtDiv" className="caseInputDiv">
+                        <button className="caseInputButt" type="submit"> <b>SUBMIT</b> </button>
+                    </div>
                 </form>  
             </div>
         )
@@ -94,3 +108,4 @@ class Case extends Component {
 }
 
 export default Case;
+
